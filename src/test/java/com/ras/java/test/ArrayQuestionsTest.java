d29aa8e66dd83b4634b32b296845d87aa99dfa08
Richard So <razs.apps@gmail.com>
2014-12-04 00:04:09 -0500

    Added more tests

--- src/test/java/com/ras/java/test/ArrayQuestionsTest.java
+++ src/test/java/com/ras/java/test/ArrayQuestionsTest.java
@@ -2,6 +2,7 @@ package com.ras.java.test;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -14,5 +15,24 @@ public class ArrayQuestionsTest {
         assertTrue("Failed for input 'Noel sees Leon'", ArrayQuestions.isPalindrome("Noel sees Leon".toCharArray()));
         assertTrue("Failed for input 'Too hot to hoot'", ArrayQuestions.isPalindrome("Too hot to hoot".toCharArray()));
         assertFalse("Failed for invalid input 'Richard So'", ArrayQuestions.isPalindrome("Richard So".toCharArray()));
+    }
+
+    @Test
+    public void testIsPermutation() {
+        assertTrue("Failed for input 'aBc12' and 'Bc1a2'", ArrayQuestions.isPermutation("aBc12".toCharArray(), "Bc1a2".toCharArray()));
+        assertTrue("Failed for input 'You are Correct!' and 'CYo !areuec rrt'", ArrayQuestions.isPermutation("You are Correct!".toCharArray(), "CYo !areuec rrt".toCharArray()));
+        assertFalse("Failed for invalid input 'You are' 'Wrong'", ArrayQuestions.isPermutation("You are".toCharArray(), "Wrong".toCharArray()));
+    }
+
+    @Test
+    public void testMakePositive() {
+        String[] input = new String[]{"This", "project", "sucks", "Rich", "sucks", null, null};
+        String[] expected = new String[]{"This", "project", "is", "awesome", "Rich", "is", "awesome"};
+        ArrayQuestions.makePositive(input);
+        assertArrayEquals(expected, input);
+        input = new String[]{"This", "question", "sucks", null};
+        expected = new String[]{"This", "question", "is", "awesome"};
+        ArrayQuestions.makePositive(input);
+        assertArrayEquals(expected, input);
     }
 }
